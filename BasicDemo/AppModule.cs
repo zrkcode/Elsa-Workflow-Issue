122bbc400316868a2998e3fcf233a32d0da3cba0
zrk <395183997@qq.com>
2020-03-28 08:34:40 +0800

    add abpvnext and elsa-core

---
+++ BasicDemo/AppModule.cs
@@ -0,0 +1,46 @@
+﻿using Elsa.Dashboard.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace BasicDemo
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreMvcModule),
+        typeof(AbpAutofacModule)
+        )]
+    public class AppModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            //context.Services.AddTransient<Elsa.Dashboard.Areas.Elsa.Controllers.HomeController>();
+            //context.Services.AddTransient<Elsa.Dashboard.Areas.Elsa.Controllers.WorkflowDefinitionController>();
+            //context.Services.AddTransient<Elsa.Dashboard.Areas.Elsa.Controllers.WorkflowInstanceController>();
+            //context.Services.AddTransient<Elsa.WorkflowDesigner.ViewComponents.WorkflowDesignerViewComponent>();
+
+            context.Services.AddMvc(options =>
+                options.EnableEndpointRouting = false
+            );
+            context.Services
+                .AddElsa()
+                .AddElsaDashboard();
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
